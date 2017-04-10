084513e9c0af77ea3d8d2844f23799402e21e9fd
afcuttin <afcuttin@gmail.com>
2017-04-10 20:35:50 +0200

    Changed variables' names according to new ones

--- test_script.m
+++ test_script.m
@@ -1,24 +1,21 @@
 % for test purposes only
 clearvars;
 
-input.sources             = 200;
-input.queueLength         = 300;
-input.linkMode            = 'tul'; %can be one of the following: 'tul', terrestrial uplink, 'sul', satellite UL, 'sdl', satellite downlink, 'tdl', terrestrial DL (type: string)
-input.rafLength           = 80;
-input.burstMaxRepetitions = 4;
-input.bitsPerSymbol       = 2;
-input.fecRate             = 4/5;
+numberOfSources           = 80;
+queueLength               = transpose(randperm(numberOfSources*2,numberOfSources));
+linkMode                  = 'tul'; %can be one of the following: 'tul', terrestrial uplink, 'sul', satellite UL, 'sdl', satellite downlink, 'tdl', terrestrial DL (type: string)
+
 % * input.sinrThreshold: value of the SINR threshold to be used (type: integer)
 
-output = randomAccess(input);
+output = randomAccess(numberOfSources,queueLength,linkMode);
 
-if strcmp(input.linkMode,'tul')
+if strcmp(linkMode,'tul')
 	% TODO: il calcolo del throughput deve essere differenziato a seconda che si usi CRDSA (slot) o CSA (slice). Per adesso sono uguali [Issue: https://github.com/afcuttin/jsac/issues/12]
-	outputLoad       = input.queueLength * input.sources ./ (input.rafLength * output.duration);
-	outputThroughput = sum(sum(output.matrix,2)) ./ (input.rafLength * output.duration);
-elseif strcmp(input.linkMode,'tdl') || strcmp(input.linkMode,'sdl') || strcmp(input.linkMode,'sul')
-	outputLoad       = input.queueLength * input.sources ./ (input.rafLength * output.duration);
-	outputThroughput = sum(sum(output.matrix,2)) ./ (input.rafLength * output.duration);
+	outputLoad       = queueLength * numberOfSources ./ (output.rafLength * output.duration);
+	outputThroughput = sum(sum(output.queues,2)) ./ (output.rafLength * output.duration);
+elseif strcmp(linkMode,'tdl') || strcmp(linkMode,'sdl') || strcmp(linkMode,'sul')
+	outputLoad       = queueLength * numberOfSources ./ (output.rafLength * output.duration);
+	outputThroughput = sum(sum(output.queues,2)) ./ (output.rafLength * output.duration);
 end
 
 fprintf('Results are stored in the "output" variable.\n');
