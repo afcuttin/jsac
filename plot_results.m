cbbd8998a50d9c249d6330e3bb366a8908deccbf
afcuttin <afcuttin@gmail.com>
2017-05-23 18:34:11 +0200

    scripts for the simulation now compete, together with plotting script

--- plot_results.m
+++ plot_results.m
@@ -1,7 +1,54 @@
 % make the plots in this file
-% TODO: evaluate load, throughput and delay for CSA with capture (4) [Issue: https://github.com/afcuttin/jsac/issues/43]
-% TODO: evaluate load, throughput and delay for CSA without capture (4) [Issue: https://github.com/afcuttin/jsac/issues/41]
-% TODO: evaluate load, throughput and delay for CRDSA with capture (4) [Issue: https://github.com/afcuttin/jsac/issues/44]
-% TODO: evaluate load, throughput and delay for CRDSA without capture (4) [Issue: https://github.com/afcuttin/jsac/issues/46]
-% TODO: evaluate throughput against load for different RAF lengths with CSA (4) [Issue: https://github.com/afcuttin/jsac/issues/45]
-% TODO: evaluate throughput against load for different RAF lengths with CRDSA (4) [Issue: https://github.com/afcuttin/jsac/issues/42]
+resultsExist = exist('results','var');
+proceed = 0;
+if resultsExist == 1
+	prompt = 'Do you want to load results different from the existing ones? y/n [n]: ';
+	str = input(prompt,'s');
+	if isempty(str)
+	    str = 'n';
+	    fprintf('I will plot existing result.\n');
+	    proceed = 1;
+	elseif str == 'y'
+		prompt = 'This will erase existing the results variable. Are you sure? y/n [n]: ';
+		str = input(prompt,'s');
+		if isempty(str) || str == 'n'
+	    	str = 'n';
+	    	fprintf('I will plot existing result.\n');
+	    	proceed = 1;
+		elseif str == 'y'
+			clear('results');
+			[filename, ~,~] = uigetfile('jsac-sim*.mat', 'Select a .mat file');
+			if ~isempty(filename)
+				proceed = 1;
+				load(filename);
+			end
+		end
+	else
+		fprintf('Please answer "y" or "n". Start again.\n');
+	end
+elseif resultsExist == 0
+	fprintf('There are no results to plot. Looking for a file to load them.\n');
+	[filename, ~,~] = uigetfile('jsac-sim*.mat', 'Select a .mat file');
+	if filename ~= 0
+		proceed = 1;
+		load(filename);
+	end
+end
+
+if proceed == 1
+	set(groot,'defaultAxesColorOrder',[1 0 0;0 1 0;0 0 1],'defaultAxesLineStyleOrder','-|--|:')
+	% plot throughput against load together with a scatterplot of the mean delay
+	for figind = unique({results.mode})
+		figure('Name',char(figind),'NumberTitle','off');
+		hold on
+		for ploind = find(strcmp(figind,{results.mode}))
+			plot(results(ploind).load, results(ploind).throughput);
+			scatter(results(ploind).load,results(ploind).throughput,40,(results(ploind).meanRetries),'filled');
+		end
+	end
+
+	set(groot,'defaultAxesLineStyleOrder','remove')
+	set(groot,'defaultAxesColorOrder','remove')
+else
+	fprintf('Exiting.\n')
+end
--- simulate.m
+++ simulate.m
@@ -1,13 +1,12 @@
 clearvars;
 
-numberOfLoadPoints = 30;
-numberOfLoadPoints = 10 % TEST: delete this line after testing
+numberOfLoadPoints = 20;
 logSpacing         = false;
 rafLengths         = [7 10 15];
 numberOfSources    = 10;
 queueLength        = 1000; % same queue length for every source
 % queueLength        = randi([500 700],numberOfSources,1); % variable queue length
-queueLength        = 200 % TEST: delete this line after testing
+queueLength        = 500 % TEST: delete this line after testing
 activeModes        = {'tul','sul'}; % a cell array with any of the following: 'tul', terrestrial uplink, 'sul', satellite UL, 'sdl', satellite downlink, 'tdl', terrestrial DL
 
 if logSpacing == 1
@@ -24,18 +23,23 @@ for modeIndex = activeModes
 		for lpIndex = 1:numberOfLoadPoints
 			inputPar.poissonThreshold = proba(lpIndex)
 			[~,~,~,~,~,~,output] = randomAccess(numberOfSources,queueLength,char(modeIndex),inputPar);
-			% TODO: il calcolo del throughput deve essere differenziato a seconda che si usi CRDSA (slot) o CSA (slice). Per adesso sono uguali [Issue: https://github.com/afcuttin/jsac/issues/12] (8)
 			if numel(queueLength) == 1
 				results(ii).load(lpIndex) = queueLength * numberOfSources ./ (output.rafLength * output.duration);
 			elseif iscolumn(queueLength)
 				results(ii).load(lpIndex) = sum(queueLength) ./ (output.rafLength * output.duration);
 			end
 			results(ii).throughput(lpIndex) = sum(sum(output.queues,2)) ./ (output.rafLength * output.duration);
+			results(ii).meanRetries(lpIndex) = mean(nonzeros(output.retries));
 			assert(results(ii).load(lpIndex) >= results(ii).throughput(lpIndex));
 			fprintf('Mode %s - Load %f Throughput %f \n',char(modeIndex),results(ii).load(lpIndex),results(ii).throughput(lpIndex));
 			validateResults(queueLength,output);
 		end
-		results(ii).mode       = char(modeIndex);
-		results(ii).rafLength  = rafIndex;
+		results(ii).mode      = char(modeIndex);
+		results(ii).rafLength = rafIndex;
 	end
 end
+
+[path,name,~] = fileparts(mfilename('fullpath'))
+datetime = datestr(now,30);
+name = strcat('jsac-sim-',datetime);
+save(fullfile(path,name),'results');
